0aa655af0b1213b69eb32c8296b35e909544aaa2
Patrick de Guzman <pdeguzman769@gmail.com>
2023-11-06 16:22:40 +0800

    updated description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -43,11 +43,12 @@ const About = () => {
 				variants={fadeIn('', '', 0.1, 1)}
 				className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
 			>
-				I am software developer graduate in Mapua Malayan Colleges Laguna as a
-				Bachelor of Science in Information Technology with experience in
-				javascript and typescript, and expertise in frameworks like React,
-				Node.js and Next.js. I am approachable and collaborative within clients
-				to create efficient, scalable and user friendly solutions.
+				I am a Software Developer graduate from Mapua Malayan Colleges Laguna
+				with a degree in Bachelor of Science in Information Technology. My
+				experiences ranges from Javascript and Typescript with expertise in
+				frameworks like React, Node.js and Next.js. <br /> <br />
+				Approachable and collaborative with clients to create efficient,
+				scalable and user friendly solutions.
 			</motion.p>
 			<div className="mt-20 flex flex-wrap gap-10">
 				{services.map((service, index) => (
